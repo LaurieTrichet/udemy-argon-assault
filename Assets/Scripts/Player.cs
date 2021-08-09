950650b08db30b3f0a99a9d9906ee23e4e1fb5b7
Laurie Trichet <laurie@twobulls.com>
2021-08-09 21:38:25 +1000

    add movement to the ship

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,41 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Vector2 direction = Vector2.zero;
+
+    public float HorizontalOffset = 1.0f;
+    public float speed = 10.0f;
+    private bool shouldMove;
+    Vector2 direction;
+    Vector3 localPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (shouldMove)
+        {
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition, direction, Time.deltaTime * speed);
+        }
     }
 
-     public void OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
-            var value = context.ReadValue<Vector2>();
-            Debug.Log(value);
+        var value = context.ReadValue<Vector2>();
+        Debug.Log(value + " , " + context.phase);
         if (context.performed)
         {
-   
+            shouldMove = true;
+            localPos = transform.localPosition;
+            direction =  new Vector2(localPos.x, localPos.y) + value * HorizontalOffset;
+        }
+        if (context.canceled)
+        {
+            shouldMove = false;
         }
     }
 
