52c640e21d0b89b488f8dab76645ae0aed6c0615
Laurie Trichet <laurie@twobulls.com>
2021-08-30 14:46:51 +1000

    add text to display the score

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
+    public TMPro.TMP_Text scoreText = null;
 
     private int score = 0;
 
     public int Score { get => score; set => score = value; }
 
+    private void Start()
+    {
+        UpdateUI();
+        
+    }
     public void UpdateScore(int points)
     {
         score += points;
+        UpdateUI();
         Debug.Log("score: " + score);
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.SetText(score.ToString());
     }
 }
